48136c59c714c97799936ac7f233afd0dbdaa34e
Yang Jinlong <yangjinlong86@gmail.com>
2021-11-13 01:01:47 +0800

    init version

---
+++ src/main/java/org/nocoder/demo/ReactSpringbootCrudApplication.java
@@ -0,0 +1,13 @@
+package org.nocoder.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactSpringbootCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactSpringbootCrudApplication.class, args);
+	}
+
+}
---
+++ src/main/java/org/nocoder/demo/repository/ClientRepository.java
@@ -0,0 +1,7 @@
+package org.nocoder.demo.repository;
+
+import org.nocoder.demo.modal.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClientRepository extends JpaRepository<Client, Long> {
+}
