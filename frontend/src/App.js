8139bcf8b852eadeb44277d265fb340466c99581
Yang Jinlong <yangjinlong86@gmail.com>
2021-11-13 02:30:48 +0800

    client crud

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,32 +1,21 @@
-import {Component} from "react";
+import React, { Component } from 'react';
+import './App.css';
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClientList from './ClientList';
+import ClientEdit from "./ClientEdit";
 
 class App extends Component {
-  state = {
-    clients: []
-  }
-
-  async componentDidMount(){
-    const response = await fetch('/clients');
-    const body = await response.json();
-    this.setState({clients: body});
-  }
-
   render() {
-    const {clients} = this.state;
-    return(
-        <div className="App">
-          <header className="App-header">
-            <div className="App-intro">
-              <h2>Clients</h2>
-              {clients.map(client =>
-                  <div key={client.id}>
-                    {client.name} ({client.email})
-                  </div>
-              )}
-            </div>
-          </header>
-        </div>
-    );
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/clients' exact={true} component={ClientList}/>
+            <Route path='/clients/:id' component={ClientEdit}/>
+          </Switch>
+        </Router>
+    )
   }
 }
 
